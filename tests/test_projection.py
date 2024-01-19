2dc35bf4a096d364a7f7196dc3a32855fc19d080
Atharva Kelkar <atharva.kelkar@gmail.com>
2024-01-19 17:29:55 +0100

    Test for G calc added

---
+++ tests/test_projection.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.append('../')
+from projection import calc_G
+import jax.numpy as jnp
+import pytest
+
+@pytest.mark.parametrize(
+    ('input_n', 'expected'),
+    (
+        (jnp.array([[1.], [1.]]), jnp.array([[8., 14.],[14., 25.]])),
+    )
+)
+
+
+def test_calc_G(input_n, expected):
+    def featurize(xx):
+        return jnp.concatenate([xx[0]**2 + xx[1]**2, 3 * xx[0] + 4 * xx[1]])
+    
+    ## Define dummy point
+    # xx = jnp.array([[1.], [1.]])
+    ## Define solution
+    # sol = jnp.array([[8., 14.],[14., 25.]])
+    ## Calculate G
+    _, G = calc_G(input_n, featurize)
+    ## Compare G to solution
+    assert jnp.all(G == expected), print(input_n.shape)
